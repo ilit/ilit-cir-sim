68314bd77fdbce80bd089c4ee59bff542041b5cb
Ilya Litvinov <ilitvinov87@gmail.com>
2014-02-10 18:44:39 +0400

    todo

--- src/ilit/cirsim/Main.java
+++ src/ilit/cirsim/Main.java
@@ -2,6 +2,9 @@ package ilit.cirsim;
 
 public class Main
 {
+    // TODO Transformer
+    // TODO Add\remove components on the fly
+
     public static void main(String[] args)
     {
         new IlitCircuitSimulator();
